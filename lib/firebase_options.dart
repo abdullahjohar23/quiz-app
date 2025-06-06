40c2cca476f27fe3549987f321552acddafdadd6
Abdullah Johar <jrrabdullah23@gmail.com>
2025-06-06 12:20:17 +0600

    indentation changed

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: type=lint
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -15,54 +15,54 @@ import 'package:flutter/foundation.dart'
 /// );
 /// ```
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+    static FirebaseOptions get currentPlatform {
+        if (kIsWeb) {
+            throw UnsupportedError(
+                'DefaultFirebaseOptions have not been configured for web - '
+                'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+        }
+        switch (defaultTargetPlatform) {
+            case TargetPlatform.android:
+                return android;
+            case TargetPlatform.iOS:
+                return ios;
+            case TargetPlatform.macOS:
+                throw UnsupportedError(
+                'DefaultFirebaseOptions have not been configured for macos - '
+                'you can reconfigure this by running the FlutterFire CLI again.',
+                );
+            case TargetPlatform.windows:
+                throw UnsupportedError(
+                'DefaultFirebaseOptions have not been configured for windows - '
+                'you can reconfigure this by running the FlutterFire CLI again.',
+                );
+            case TargetPlatform.linux:
+                throw UnsupportedError(
+                'DefaultFirebaseOptions have not been configured for linux - '
+                'you can reconfigure this by running the FlutterFire CLI again.',
+                );
+            default:
+                throw UnsupportedError(
+                'DefaultFirebaseOptions are not supported for this platform.',
+                );
+        }
     }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
-    }
-  }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyC2lX20H2_L_q6Kc2LXypT9A4TFjKuEiOk',
-    appId: '1:392187918962:android:7f563598b429aadc3760eb',
-    messagingSenderId: '392187918962',
-    projectId: 'quizapp-4e37a',
-    storageBucket: 'quizapp-4e37a.firebasestorage.app',
-  );
+    static const FirebaseOptions android = FirebaseOptions(
+        apiKey: 'AIzaSyC2lX20H2_L_q6Kc2LXypT9A4TFjKuEiOk',
+        appId: '1:392187918962:android:7f563598b429aadc3760eb',
+        messagingSenderId: '392187918962',
+        projectId: 'quizapp-4e37a',
+        storageBucket: 'quizapp-4e37a.firebasestorage.app',
+    );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAF8pqTP5yomTVMI1FIt8WjlIefz9kamtU',
-    appId: '1:392187918962:ios:d9a6e7ad84e325ec3760eb',
-    messagingSenderId: '392187918962',
-    projectId: 'quizapp-4e37a',
-    storageBucket: 'quizapp-4e37a.firebasestorage.app',
-    iosBundleId: 'com.example.quizApp',
-  );
+    static const FirebaseOptions ios = FirebaseOptions(
+        apiKey: 'AIzaSyAF8pqTP5yomTVMI1FIt8WjlIefz9kamtU',
+        appId: '1:392187918962:ios:d9a6e7ad84e325ec3760eb',
+        messagingSenderId: '392187918962',
+        projectId: 'quizapp-4e37a',
+        storageBucket: 'quizapp-4e37a.firebasestorage.app',
+        iosBundleId: 'com.example.quizApp',
+    );
 }
