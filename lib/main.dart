784d37c1bec40b9b7389cd1deec84c0fc22ef3eb
Abdullah Johar <jrrabdullah23@gmail.com>
2025-06-10 15:21:54 +0600

    dotenv load

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/firebase_options.dart';
 import 'package:quiz_app/pages/admin_login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
+    
+    await dotenv.load(fileName: 'secrets.env'); // Loads the .env file
+    // If your secret file is named secrets.env (not the default .env), then you must specify the filename explicitly when loading it
+    // If you just write await dotenv.load(); without a filename, it tries to load the default .env file — so it won’t find your secrets.env automatically
+
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform
     );
